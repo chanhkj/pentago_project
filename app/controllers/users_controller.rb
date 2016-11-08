d9a398dedb246057c647f94302ed48557d2a869f
Hou Kan Julian Chan <julianchanhk@gmail.com>
2016-11-08 09:24:37 +0800

    first initialisation

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
